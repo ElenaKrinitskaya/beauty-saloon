8e526706623201f52037d1eb73aafbc560ebe3e3
ElenaKrinitskaya <eakrinitskaya@yandex.ru>
2022-10-30 23:44:45 +0700

    Sprint5 is ready

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,13 @@ const cssnano = require('cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const clean = require('gulp-clean');
+const webpackStream = require('webpack-stream');
+const rename = require('gulp-rename');
 
 
 // Таск компиляции SASS в CSS
 function buildSass() {
-    return src('src/scss/**/*.scss')
+    return src('site/src/scss//*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({includePaths: ['./node_modules']}).on('error', sass.logError))
         .pipe(
@@ -23,21 +25,21 @@ function buildSass() {
             ])
         )
         .pipe(sourcemaps.write())
-        .pipe(dest('src/css'))
-        .pipe(dest('dist/css'))
+        .pipe(dest('site/src/css'))
+        .pipe(dest('site/dist/css'))
         .pipe(browserSync.stream());
 }
 
 // Таск работы с html файлами
 function buildHtml() {
-    return src('src/**/*.html')
+    return src('site/src//*.html')
         .pipe(dest('dist'))
         .pipe(browserSync.stream());
 }
 
 // Таск копирования статичных файлов
 function copy() {
-    return src(['src/images/**/*.*'], { base: 'src' }).pipe(dest('dist'));
+    return src(['site/src/images//*.*'], { base: 'src' }).pipe(dest('dist'));
 }
 
 // Таск очистки dist
@@ -47,10 +49,19 @@ function cleanDist() {
 
 // Таск отслеживания изменения файлов
 function serve() {
-    watch('src/scss/**/*.scss', buildSass);
-    watch('src/**/*.html', buildHtml);
+    watch('site/src/scss//*.scss', buildSass);
+    watch('site/src//*.html', buildHtml);
+    watch(['site/src/js//*.js', '!site/src/js/**/*.min.js'], buildJs);
 }
 
+function buildJs() {
+    return src('site/src/js/script.js')
+        .pipe(webpackStream(require('./webpack.config')))
+        .pipe(rename('main.min.js'))
+        .pipe(dest('site/src/js'))
+        .pipe(dest('site/dist/js'))
+        .pipe(browserSync.stream());
+}
 
-exports.build = series(cleanDist, buildSass, buildHtml, copy);
-exports.default = series(buildSass, parallel(serve));
+exports.build = series(cleanDist, parallel(buildSass, buildHtml, copy, buildJs));
+exports.default = series([buildSass, buildJs], parallel(serve));
